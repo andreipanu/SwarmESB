e9481d342a9f84ef8f740501cc5875a23d676f0d
Andrei Panu <andrei_panu@yahoo.com>
2014-09-24 08:19:20 +0300

    finished MapReduce implementation

--- swarms/MapReduceSwarm.js
+++ swarms/MapReduceSwarm.js
@@ -8,17 +8,11 @@ var mapReduceSwarm =
     vars:{
         documentContent:"",
         dividedInput:null,
-        mapWorkers:null,
-        reduceWorkers:null,
-        mapPhaseOutputList:null,
         debug:"false"
     },
     doWork:function(documentContent) {
         this.documentContent = documentContent;
         this.dividedInput = [];
-        this.mapWorkers = [];
-        this.reduceWorkers = [];
-        this.mapPhaseOutputList = [];
         this.swarm("splitInput");
     },
     splitInput:{
@@ -31,106 +25,157 @@ var mapReduceSwarm =
                 slice[arrayOfSentences[i].trim().toLowerCase()] = 1;
                 this.dividedInput.push(slice);
             }
-            console.log(">>>>> splitted input in key/value pairs (it is prepared for map phase)");
+            console.log("> splitted input in key/value pairs (it is prepared for map phase)");
             this.documentContent = null; // we don't need to carry on the original input data
-            this.swarm("getWorkerNodes");
+            this.swarm("distributeLoad");
         }
     },
-    getWorkerNodes:{
+    distributeLoad:{
         node:"Balancer",
         code:function() {
-            this.mapWorkers = getAllMapWorkers();
-            this.reduceWorkers = getAllReduceWorkers();
-            this.swarm("executeMapPhase");
+            var mapWorkers = getAllMapWorkers();
+            var reduceWorkers = getAllReduceWorkers();
+            var n = 0;
+            var m = 0;
+
+            var ctx = getContext("balancer");
+            ctx.finishedReducerJobs = 0;
+            ctx.reducersResponseCounter = 0;
+            ctx.finalResults = [];
+
+            this.chunkCount = this.dividedInput.length;
+            this.currentChunk = 0;
+            this.chosenReduceWorker = "";
+            
+            for (this.currentChunk = 0; this.currentChunk < this.dividedInput.length; this.currentChunk++) {
+                var chosenMapWorker = mapWorkers[n % mapWorkers.length];
+                this.chosenReduceWorker = reduceWorkers[m % reduceWorkers.length];
+                n++;
+                if (n == mapWorkers.length) { n = 0; }
+                m++;
+                if (m == reduceWorkers.length) { m = 0; }
+                console.log(">>>>> [Balancer] sent swarm to worker " + chosenMapWorker + " and reducer " + this.chosenReduceWorker);
+
+                this.swarm("executeMapPhase", chosenMapWorker);
+
+            }
         }
     },
     executeMapPhase:{
-        node:"ClientAdapter",
+        node:"*",
         code:function() {
-            //
-            // create a swarm for each key/value pair and send it to a specific worker;
-            // this following approach/implementation is not ok, we should directly send a swarm 
-            // from here, but the function startSwarm() doesn't emit an event when the result is returned
-            //
-            var sutil = require('swarmutil');
-            var adapterPort         = 3000;
-            var adapterHost         = "localhost";
-            swarmSettings.authentificationMethod = "testCtor";
-            globalVerbosity = false;
-            var noOfReceivedResults = 0;
-            var noOfSentJobs = 0;
-            var mapPhaseOutput = [];
-
-            var j = 0;
-            for (var i = 0; i < this.dividedInput.length; i++) {
-                var job = { input: {}, workerName: "" };
-                job.input = this.dividedInput[i];
-                job.workerName = this.mapWorkers[j];
-                sendMapSwarm(job);
-                noOfSentJobs++;
-                if (j == this.mapWorkers.length - 1) {
-                    j = 0;
-                } else {
-                    j++;
-                }
-            }
-
-            this.swarm("executeReducePhase");
+            console.log(">>>>>>>>>> [" + thisAdapter.nodeName + "] processing chunk: " + 
+                this.currentChunk + " with data: " + Object.keys(this.dividedInput[this.currentChunk]));
             
-            function sendMapSwarm(job) {
-                var client = sutil.createClient(adapterHost, adapterPort, "UserForStartSwarmTest", "ok","BalancerTest");
-                client.startSwarm("MapWorkerSwarm.js", "doWork", job);
-                client.on("MapWorkerSwarm.js", getMapResults);
-                console.log(">>>>> sent to worker: " + job.workerName + " the input: " + Object.keys(job.input));
-            }
-
-            function getMapResults(results){
-                //console.log(">>>>> received the following results" + results.wordAppearancesArray);
-                mapPhaseOutput = mapPhaseOutput.concat(results.wordAppearancesArray);
-                noOfReceivedResults++;
-                if (noOfReceivedResults == noOfSentJobs) { // we have received the results from all map workers
-                    console.log(">>>>> map phase finished; we have received " + mapPhaseOutput.length + " objects");
-                    startReducePhase(mapPhaseOutput);
+            var arrayOfWords = Object.keys(this.dividedInput[this.currentChunk])[0].split(" ");
+            this.mapPhaseResults = [];
+            for (var i = 0; i < arrayOfWords.length; i++) {
+                // execute local reduce
+                var found = false;
+                for (var j = 0; j < this.mapPhaseResults.length; j++) { // search if the word already exists
+                    if (arrayOfWords[i] == Object.keys(this.mapPhaseResults[j])[0]) { // the word already exists, just increment
+                        this.mapPhaseResults[j][arrayOfWords[i]] += 1;
+                        found = true;
+                    }
+                }
+                if (!found) { // it's a new word
+                    var obj = {};
+                    obj[arrayOfWords[i]] = 1;
+                    this.mapPhaseResults.push(obj);
                 }
             }
-
-            function startReducePhase(output) {
-                this.mapPhaseOutputList = output;
-                var fs = require('fs');
-                fs.writeFileSync('done','');
-                console.log(">>>>> created file");
-                //this.swarm("executeReducePhase"); // here we cannot call this function
-            }
+            
+            console.log(">>>>>>>>>>>>>>> [" + thisAdapter.nodeName + "] created array with " + this.mapPhaseResults.length + 
+                " objects; sending it to reducer: " + this.chosenReduceWorker);
+            
+            this.swarm("executeReducePhase", this.chosenReduceWorker);
         }
     },
     executeReducePhase:{
-        node:"ClientAdapter",
+        node:"*",
         code:function() {
-
-            var fs = require('fs');
-            while (!fs.existsSync("done")) { }
-            console.log(">>>>> file found, continuing...");
-
-            /*var sutil = require('swarmutil');
-            var adapterPort         = 3000;
-            var adapterHost         = "localhost";
-            swarmSettings.authentificationMethod = "testCtor";
-            globalVerbosity = false;
-
-            var uniqueKeysArray = buildUniqueKeysArray();
+            console.log(">>>>>>>>>>>>>>>>>>>> [" + thisAdapter.nodeName + "] received words array with " + this.mapPhaseResults.length + " objects");
             
-            function buildUniqueKeysArray() {
-                var keys = [];
-                for (var i = 0; i < this.mapPhaseOutputList.length; i++) {
-                    var key = Object.keys(mapPhaseOutputList[i])[0];
-                    if (!keys.contains(key)) {
-                        keys.push(key);
+            var ctx = getContext("reducerResults");
+            if (ctx.partialResults == null || ctx.partialResults == undefined) {
+                ctx.partialResults = this.mapPhaseResults;
+            } else {
+                // TODO: move this code into a function into the reducer
+                for (var i = 0; i < this.mapPhaseResults.length; i++) {
+                    var word = Object.keys(this.mapPhaseResults[i])[0];
+                    var found = false;
+                    for (var j = 0; j < ctx.partialResults.length; j++) {
+                        if (word == Object.keys(ctx.partialResults[j])[0]) {
+                            ctx.partialResults[j][word] += 1;
+                            found = true;
+                        }
+                    }
+                    if (!found) {
+                        ctx.partialResults.push(this.mapPhaseResults[i]);
                     }
                 }
-                console.log(">>>>> we have identified " + keys.length + " unique keys");
-            }*/
+            }
+            
+            console.log(">>>>>>>>>>>>>>>>>>>> [" + thisAdapter.nodeName + "] partial results array with " + ctx.partialResults.length + " objects");
+
+            this.swarm("checkIfFinished");
         }
-    }    
+    },
+    checkIfFinished:{
+        node:"Balancer",
+        code:function() {
+            var ctx = getContext("balancer");
+            ctx.finishedReducerJobs++;
+
+            if (ctx.finishedReducerJobs == this.chunkCount) {
+                console.log("> all reducers have finished their jobs");
+                var reduceWorkers = getAllReduceWorkers();
+                for (var i = 0; i < reduceWorkers.length; i++) {
+                    this.swarm("getPartialFinalResults", reduceWorkers[i]);
+                }
+            }
+        }
+    },
+    getPartialFinalResults:{
+        node:"*",
+        code:function() {
+            var ctx = getContext("reducerResults");
+            this.reducerPartialFinalResults = ctx.partialResults;
+            ctx.partialResults = null;
+            this.swarm("finalPhase");
+        }
+    },
+    finalPhase:{
+        node:"Balancer",
+        code:function() {
+            console.log("> received from reducer an array with " + this.reducerPartialFinalResults.length + " objects");
+            var reduceWorkers = getAllReduceWorkers();
+            var ctx = getContext("balancer");
+            ctx.finalResults = ctx.finalResults.concat(this.reducerPartialFinalResults);
+            ctx.reducersResponseCounter++;
+
+            if (ctx.reducersResponseCounter == reduceWorkers.length) {
+                this.results = [];
+                // TODO: move this code into a function into the balancer
+                for (var i = 0; i < ctx.finalResults.length; i++) {
+                    var word = Object.keys(ctx.finalResults[i])[0];
+                    var found = false;
+                    for (var j = 0; j < this.results.length; j++) {
+                        if (word == Object.keys(this.results[j])[0]) {
+                            this.results[j][word] += ctx.finalResults[i][word];
+                            found = true;
+                        }
+                    }
+                    if (!found) {
+                        this.results.push(ctx.finalResults[i]);
+                    }
+                }
+
+                console.log("> job finished, sending final results to client");
+                this.home("results");
+            }
+        }
+    }  
 };
 
 mapReduceSwarm;
--- test/MapReduceClientTest.js
+++ test/MapReduceClientTest.js
@@ -17,18 +17,23 @@ fs.readFile(INPUT_FILE, 'utf8', function(err,fileContent) {
 
 	var client = util.createClient(adapterHost, adapterPort, "UserForStartSwarmTest", "ok","BalancerTest");
 
-	client.startSwarm("MapReduceSwarm.js","doWork",fileContent);
+	client.startSwarm("MapReduceSwarm.js", "doWork", fileContent);
 
-	client.on("MapReduceSwarm.js",getGreetings);
+	client.on("MapReduceSwarm.js", displayResults);
 })
 
-function getGreetings(obj){
-    cprint("Work finished in " + obj.selectedWorker);
+function displayResults(response) {
+    var results = response.results;
+    for (var i = 0; i < results.length; i++) {
+    	var word = Object.keys(results[i])[0];
+    	console.log(word + " : " + results[i][word]);
+    }
+    process.exit(1);
 }
 
 setTimeout (
     function() {
         process.exit(1);
     },
-    3000
+    10000
 );
